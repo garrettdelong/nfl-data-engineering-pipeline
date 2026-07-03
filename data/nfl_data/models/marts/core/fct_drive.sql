be5b31431b26f819fdea5bc96464172f02e3a49d
AmratheAvenger <apecxamra@gmail.com>
2026-07-02 17:15:58 -0700

    Revert fct_drive incremental change

--- data/nfl_data/models/marts/core/fct_drive.sql
+++ data/nfl_data/models/marts/core/fct_drive.sql
@@ -1,9 +1,3 @@
-{{ config(
-    materialized = 'incremental',
-    unique_key = 'drive_key',
-    incremental_strategy = 'merge',
-    on_schema_change = 'sync_all_columns'
-) }}
 
 WITH plays AS (
   SELECT
@@ -15,27 +9,13 @@ WITH plays AS (
     fct_play.epa,
     fct_play.success,
     stg_pbp.drive_inside20,
-    stg_pbp.drive_ended_with_score,
-    fct_play.loaded_at
+    stg_pbp.drive_ended_with_score
   FROM {{ ref('fct_play') }} AS fct_play
     INNER JOIN {{ ref('stg_play_by_play') }} AS stg_pbp
       ON stg_pbp.game_id = fct_play.game_id
       AND stg_pbp.play_id = fct_play.play_id
     WHERE fct_play.drive IS NOT NULL
     AND fct_play.play_type != 'no_play'
-    {% if is_incremental() %}
-    AND fct_play.loaded_at >= DATEADD(
-      DAY,
-      -{{ var('incremental_lookback_days', 3) }},
-      COALESCE(
-        (
-          SELECT MAX(loaded_at)
-          FROM {{ this }}
-        ),
-        CAST('1900-01-01' AS TIMESTAMP)
-      )
-    )
-    {% endif %}
 ),
 
 drive_agg AS (
@@ -48,8 +28,7 @@ drive_agg AS (
     MAX(drive_inside20) AS drive_inside20,
     MAX(drive_ended_with_score) AS drive_ended_with_score,
     SUM(epa) AS epa_sum,
-    AVG(success) AS success_rate,
-    MAX(loaded_at) AS loaded_at
+    AVG(success) AS success_rate
     FROM plays
     GROUP BY
       game_id,
@@ -57,7 +36,6 @@ drive_agg AS (
 )
 
 SELECT
-  drive_agg.game_id || '-' || drive_agg.drive AS drive_key,
   drive_agg.game_id,
   drive_agg.drive,
   drive_agg.offense_franchise_id,
@@ -66,6 +44,5 @@ SELECT
   drive_agg.epa_sum,
   drive_agg.success_rate,
   drive_agg.drive_inside20,
-  drive_agg.drive_ended_with_score,
-  drive_agg.loaded_at
+  drive_agg.drive_ended_with_score
 FROM drive_agg
