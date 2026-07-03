ad516589fa22254c9c7df2ea23abf0c0f5f3f20c
AmratheAvenger <apecxamra@gmail.com>
2026-07-02 17:06:32 -0700

    incremental for fct_drive

--- data/nfl_data/tests/fct_drive_unique_game_id_drive.sql
+++ data/nfl_data/tests/fct_drive_unique_game_id_drive.sql
@@ -1,9 +1,7 @@
 SELECT
-  fct_drive.game_id,
-  fct_drive.drive,
+  fct_drive.drive_key,
   COUNT(*) AS row_count
 FROM {{ ref('fct_drive') }} AS fct_drive
 GROUP BY
-  fct_drive.game_id,
-  fct_drive.drive
+  fct_drive.drive_key
 HAVING COUNT(*) > 1
